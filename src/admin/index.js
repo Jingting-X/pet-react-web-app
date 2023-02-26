570391a66a93964ac8519ac4ed1c86a8523a042c
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-02-25 22:50:56 -0500

    admin page setup

---
+++ src/admin/index.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+
+const AdminNav = () => {
+    return (
+        <div className="container mt-2 w-75">
+            <ul className="nav nav-tabs border-0">
+                <li className="nav-item">
+                    <a class="nav-link active text-white fw-bold bg-primary" href="#">User List</a>
+                </li>
+                <li className="nav-item">
+                    <a class="nav-link" href="#">Post List</a>
+                </li>
+            </ul>
+            <ul className="list-group rounded-0">
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"/>
+                    <i className="fa fa-pencil me-3"/>
+                </li>
+
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+                <li className="list-group-item d-flex align-items-end">
+                    <span className="me-auto">Alice</span>
+                    <i className="fa fa-trash me-3"></i>
+                    <i className="fa fa-pencil me-3"></i>
+                </li>
+
+
+            </ul>
+        </div>
+        );
+}
+ export default AdminNav;
