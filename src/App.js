71e772f4421afc10e872b4a1d39abfdc86395750
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-04-05 13:40:42 -0400

    admin page setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
-import {Routes, Route} from 'react-router';
+import {Route, Routes} from 'react-router';
 import Home from "./home";
 import AdminComponent from "./admin";
+import Footnote from "./home/Footnote";
+import './styles/app.css'
 
 function App() {
-  return(
-      <BrowserRouter>
-        <div className="container">
-          <Routes>
-            <Route path="/home/*" element={<Home/>}/>
-            <Route path="/admin" element={<AdminComponent/>}/>
-          </Routes>
-        </div>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <div className="text-center fw-bold mt-2"
+                     style={{fontFamily: 'cursive', fontSize: '80px', color: 'lightpink'}}>
+                    Dog Land
+                </div>
+                <div className="content-wrapper">
+                    <Routes>
+                        <Route path="/home/*" element={<Home/>}/>
+                        <Route path="/admin" element={<AdminComponent/>}/>
+                    </Routes>
+                </div>
+                <div>
+                    <Footnote/>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
+
 export default App;
--- src/home/Footnote/index.js
+++ src/home/Footnote/index.js
@@ -1,45 +1,46 @@
 import React from 'react';
 
 const Footnote = () => {
-  return(
-      <div style={{backgroundColor: 'black'}}>
-        <div className="row">
-          <ul className="col-sm-6 link-light" style={{width: '450px'}}>
-            <div className="float-end me-5 mt-4">
-              <li style={{listStyleType: 'none'}}>
-                <p className="ps-5" style={{fontFamily: 'cursive', fontSize: '50px', color: 'lightpink'}}>Dog Land</p>
-              </li>
-              <li style={{listStyleType: 'none'}}>
-                <span className="text-white ps-5 small">Produced by ...</span>
-              </li>
-              <li style={{listStyleType: 'none'}}>
-                <span className="text-white ps-5 small">2023, Dog Land</span>
-              </li>
-              <li style={{listStyleType: 'none'}}>
-                <span className="text-white ps-5 mb-5 small">All right reserved</span>
-              </li>
+    return (
+        <div className="wd-footer">
+            <div className="row h-75 mt-5 pb-0">
+                <ul className="col-sm-6 link-dark" style={{width: '450px'}}>
+                    <div className="float-end me-5 mt-4">
+                        <li style={{listStyleType: 'none'}}>
+                            <p className="ps-5" style={{fontFamily: 'cursive', fontSize: '50px', color: 'darkpink'}}>Dog
+                                Land</p>
+                        </li>
+                        <li style={{listStyleType: 'none'}}>
+                            <span className="text-black ps-5 small">Produced by ...</span>
+                        </li>
+                        <li style={{listStyleType: 'none'}}>
+                            <span className="text-black ps-5 small">2023, Dog Land</span>
+                        </li>
+                        <li style={{listStyleType: 'none'}}>
+                            <span className="text-black ps-5 mb-5 small">All right reserved</span>
+                        </li>
+                    </div>
+                </ul>
+
+                <ul className="col-sm-6 mt-5" style={{width: '250px', listStyleType: 'none'}}>
+                    <li className="mb-4"><a className="link-dark" href="#">About us</a></li>
+                    <li className="mb-4"><a className="link-dark" href="#">Contact us</a></li>
+                </ul>
+
+                <ul className="col-sm-6 mt-5" style={{width: '250px', listStyleType: 'none'}}>
+                    <li className="mb-4"><a className="link-dark" href="#">Blog</a></li>
+                    <li className="mb-4"><a className="link-dark" href="#">Event</a></li>
+                    <li className="mb-4"><a className="link-dark" href="#">Setting</a></li>
+                    <li className="mb-4"><a className="link-dark" href="#">Notifications</a></li>
+                </ul>
+
+                <ul className="col-sm-6 mt-5" style={{width: '250px', listStyleType: 'none'}}>
+                    <li className="mb-4"><a className="link-dark" href="#">Terms of Use</a></li>
+                    <li className="mb-4"><a className="link-dark" href="#">Privacy Policy</a></li>
+                    <li className="mb-4"><a className="link-dark" href="#">Q&A</a></li>
+                </ul>
             </div>
-          </ul>
-
-          <ul className="col-sm-6 mt-5" style={{width: '250px', listStyleType: 'none'}}>
-            <li className="mb-4"><a className="link-light" href="#">About us</a></li>
-            <li className="mb-4"><a className="link-light" href="#">Contact us</a></li>
-          </ul>
-
-          <ul className="col-sm-6 mt-5" style={{width: '250px', listStyleType: 'none'}}>
-            <li className="mb-4"><a className="link-light" href="#">Blog</a></li>
-            <li className="mb-4"><a className="link-light" href="#">Event</a></li>
-            <li className="mb-4"><a className="link-light" href="#">Setting</a></li>
-            <li className="mb-4"><a className="link-light" href="#">Notifications</a></li>
-          </ul>
-
-          <ul className="col-sm-6 mt-5" style={{width: '250px', listStyleType: 'none'}}>
-            <li className="mb-4"><a className="link-light" href="#">Terms of Use</a></li>
-            <li className="mb-4"><a className="link-light" href="#">Privacy Policy</a></li>
-            <li className="mb-4"><a className="link-light" href="#">Q&A</a></li>
-          </ul>
         </div>
-      </div>
-);
+    );
 }
 export default Footnote;
