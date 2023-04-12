8eb5dd9fd2511774a160bf6618b85f0739d7ba71
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-04-11 20:57:43 -0400

    signup page

---
+++ src/components/modal.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "../styles/app.css";
+
+function Modal({ show, message, onClose }) {
+    if (!show) return null;
+
+    return (
+        <div className="modal centered-modal" >
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title">Error</h5>
+                        <button type="button" className="btn-close" onClick={onClose}></button>
+                    </div>
+                    <div className="modal-body">
+                        <p>{message}</p>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" onClick={onClose}>
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
