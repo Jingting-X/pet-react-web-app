1b039c7663aea02f8bfd0bd06497459426e3868d
Lili Qing <lili.lily.qing@gmail.com>
2023-04-12 11:26:44 -0700

    add user info on detail page

--- src/dogs/dogs-detail.js
+++ src/dogs/dogs-detail.js
@@ -1,8 +1,11 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {fullTextSearch, getDetail} from "./dogs-service";
+import {useSelector} from "react-redux";
 
 function DogsDetailScreen() {
+    const {currentUser} = useSelector((state => state.users))
+    console.log("current User is:", currentUser);
     const {id} = useParams();
     console.log("In dogs-detail image id is:", id);
     const [detail, setDetail] = useState({});
@@ -20,8 +23,14 @@ function DogsDetailScreen() {
     return (
         <div>
             <h1>Dogs detail page</h1>
+            <h1>Current user: {currentUser.firstName} {currentUser.lastName}</h1>
+            <h1>Current user id: {currentUser._id}</h1>
             <p>{detail.name}</p>
             <img src={`https://cdn2.thedogapi.com/images/${id}.jpg`} width={400} height={300} alt={id}/>
+            <div>
+            <button className="btn btn-success">Like</button>
+            <button className="btn btn-danger">Dislike</button>
+            </div>
             {/*<pre>{JSON.stringify(detail, null, 2)}</pre>*/}
         </div>
     )
