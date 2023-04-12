3c5bc598034fdfc91b15302a4b3d1d45cc0a68fe
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-04-12 14:46:43 -0400

    delete a user from user management by admin

--- src/screens/users-screen.js
+++ src/screens/users-screen.js
@@ -1,26 +1,22 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
-import { useNavigate } from 'react-router';
 import { getUsersThunk, deleteUserThunk } from '../services/users-thunks.js';
 function UsersScreen() {
-    const { currentUser, users } = useSelector(state => state.users);
-    const navigate = useNavigate();
+    const {users} = useSelector(state => state.users);
+    
     const dispatch = useDispatch();
-
-    // if (!currentUser || currentUser.role !== 'admin') {
-    //     navigate('/signin');
-    // }
 
     useEffect(() => {
         dispatch(getUsersThunk());
     }, []);
 
-    const handleDeleteUserById = (_id) => {
-        dispatch(deleteUserThunk(_id));
+    const handleDeleteUserById = async (_id) => {
+        await dispatch(deleteUserThunk(_id));
+        dispatch(getUsersThunk());
     }
     return (
         <ul className='list-group mt-3'>
-            {users && users.map(user => (
+            {users.map(user => (
                 <li className='list-group-item' key={user._id}>
                     {user.firstName} {user.lastName}
                     <button className='btn btn-danger float-end me-3'
