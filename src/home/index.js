dcbeffccfe492f21bbb358b5a6851d6ced10c735
Eva Yang <evayang@Jies-MacBook-Pro.local>
2023-04-14 11:00:39 -0400

    comment admin related components in home page to avoid blocking other teammates

--- src/home/index.js
+++ src/home/index.js
@@ -11,15 +11,15 @@ import { useSelector } from 'react-redux';
 
 
 function Home() {
-  const { currentUser } = useSelector((state) => state.users);
+  // const { currentUser } = useSelector((state) => state.users);
 
   return (
         <div className="container mb-4">
           <div className="row mt-2">
             <div className="col-2 col-md-2 col-lg-2 col-xl-2">
               {/*<NavigationSidebarGuest/>*/}
-              {/*<NavigationSidebarUser/>*/}
-              {currentUser.role === "admin" && <NavigationSidebarAdmin/>}
+              <NavigationSidebarUser/>
+              {/* {currentUser.role === "admin" && <NavigationSidebarAdmin/>} */}
             </div>
             <div className="col-10 col-md-7 col-lg-7 col-xl-7" style={{"position": "relative"}}>
               <Routes>
@@ -28,7 +28,7 @@ function Home() {
             </div>
             <div className="d-none d-sm-none d-md-none d-lg-block col-lg-3 col-xl-3">
               {/*<Calendar/>*/}
-              {currentUser.role === "admin" &&<WelcomeAdmin/>}
+              {/* {currentUser.role === "admin" &&<WelcomeAdmin/>} */}
               {/*<WhoToFollowList/>*/}
             </div>
           </div>
