57fa27f5f88edb47c159e0edc2eb82c1a0611f50
Jzhou271 <zhou.jing4@northeastern.edu>
2023-04-21 16:55:40 -0700

    event components done on home page

--- src/home/calendar-list/calendar-list-item.js
+++ src/home/calendar-list/calendar-list-item.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
-const EventSummaryItem = ({post}) => {
-  return(
+const EventSummaryItem = ({ post }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails((prevState) => !prevState);
+  };
+
+  return (
       <li className="list-group-item">
         <div className="row">
           <div className="col-12">
-            <div className="fw-bold" style={{color: "palevioletred", fontSize: 20}}>{post.title}</div>
+            <div
+                className="fw-bold"
+                style={{ color: "palevioletred", fontSize: 20 }}
+            >
+              {post.title}
+            </div>
             <div className="fw-bold small">{post.time}</div>
-            <div className="text-muted">{post.event}</div>
+            <div
+                className="text-muted"
+                style={{ maxHeight: showDetails ? "none" : "3em", overflow: "hidden" }}
+            >
+              {post.event}
+            </div>
+
+            <button
+                className="btn btn-link float-end"
+                onClick={toggleDetails}
+                style={{ display: showDetails || post.event.length <= 150}}
+            >
+              {showDetails ? "Show less" : "Show more"}
+            </button>
+
           </div>
         </div>
       </li>
   );
 };
+
 export default EventSummaryItem;
+
