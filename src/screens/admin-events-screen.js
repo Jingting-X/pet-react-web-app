608e20b543aafac6dcf3a83a0a24a78f0661b4f2
Jingting <xingjingting1217@gmail.com>
2023-04-22 21:47:22 -0700

    change admin to Admin

--- src/screens/admin-events-screen.js
+++ src/screens/admin-events-screen.js
@@ -27,7 +27,7 @@ function AdminEventsScreen() {
 
     return (
         <>
-        {!currentUser || currentUser.role !== "admin" ? (
+        {!currentUser || currentUser.role !== "Admin" ? (
             <div className='container bg-light p-4 mb-5'>
                 <h2>You are not allowed to browse this page, please <a href='/signin'>sign in</a>.</h2>
                 <div style={{ height: '85px' }}>
